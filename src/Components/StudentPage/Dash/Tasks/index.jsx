ed52ecc0852373be3415d40fbe2ccb0e74909fe2
AnthonyWright17 <awright032317@gmail.com>
2022-08-03 15:56:20 -0600

    changes

--- src/Components/StudentPage/Dash/Tasks/index.jsx
+++ src/Components/StudentPage/Dash/Tasks/index.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { useEffect } from 'react'
+import {React, useState, useContext} from 'react'
+import StudentContext from '../../../../Context/StudentPageContext'
 
 export const Tasks = () => {
+  const {student} = useContext(StudentContext)
+  const [tasks, setTasks] = useState([])
+
+  
+  const getStudentTasks =  (studentId) => {
+    fetch(`https://hacking-transition.herokuapp.com/api/tasks/student/${studentId}`).then(res => res.json()).then(data => setTasks(data))
+  }
+  
+  useEffect(() => {
+    if(student[0]){
+      let studentId = parseInt(student.user_id);
+      getStudentTasks(studentId)
+    }
+    
+  }, [student])
+
   return (
-    <h1>Tasks</h1>
+    <ul>
+      <h1>Tasks</h1>
+      {tasks.map((task)=> {return <li>{task.title}</li>}
+      )}
+    </ul>
   )
 }
 
